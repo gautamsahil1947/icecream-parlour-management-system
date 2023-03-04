cc0e7db92e48437f247d3056396638c33483fc5d
sahil <gautamsahil1947>
2023-03-04 11:56:50 +0530

    base files

---
+++ functions.c
@@ -0,0 +1,31 @@
+void enter_system(void)
+{
+    
+
+}
+
+void restock(void)
+{
+
+}
+
+void check_stock(void)
+{
+
+}
+
+void stock_summary(void)
+{
+
+}
+
+int lexicographic_order(char *a, char *b)
+{
+    return 0;
+}
+
+void electricity_bill(void)
+{
+
+
+}
---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "functions.c"
+#include <time.h>
+
+// amount for transactions
+#define default_amount 10000;
+int money = default_amount;
+
+float uptime = 0;
+
+// linked list struct for stroing the icecream data
+typedef struct ice_cream_inventory
+{
+    char * name;
+    int quantity;
+    struct ice_cream_inventory *next;
+}
+ice_cream_inventory;
+
+
+
+void main(void)
+{
+    // first interaction
+    int n;
+    printf("enter 1 to start system, 0 to exit: ");
+    scanf("%i", &n);
+    
+    // enter the system
+    if (n == 1)
+    {
+        enter_system();
+    }
+    else if (n == 0)
+    {
+        // summarise stock, money, electricity bill etc.
+        stock_summary();
+        printf("thank you for using the system.\n");
+    }
+    else
+    {
+        main();
+    }
+
+
+}
